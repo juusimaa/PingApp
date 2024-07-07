2f45b3c1d72a1ce82449fc5a74365711faa6f888
Jouni Uusimaa <jouni.uusimaa@gmail.com>
2024-07-07 12:08:54 +0300

    Draft.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using PingApp.Components;
+using PingApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<PingService>();
 
 var app = builder.Build();
 
---
+++ Services/PingService.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using System.Net.NetworkInformation;
+
+namespace PingApp.Services
+{
+    public sealed class PingService
+    {
+        private readonly Ping _pinger = new();
+
+        internal long Ping([CanBeNull]string address)
+        {
+            if (string.IsNullOrEmpty(address))
+            {
+                return default;
+            }
+
+            try
+            {
+                return _pinger.Send(address).RoundtripTime;                
+            }
+            catch (PingException)
+            {
+                return default;
+            }
+        }
+    }
+}
